5237caa4c02b5417e272c89305efe272d69bb204
alpsakaci <erenalpsakaci@gmail.com>
2022-06-07 13:09:51 +0300

    init project

---
+++ main.py
@@ -0,0 +1,8 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/", StaticFiles(directory="static"), name="static")
